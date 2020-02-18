17b4fc335f81732f16764a240775ca5b8fdb004a
b66957am <amir.maula@student.manchester.ac.uk>
2020-02-18 17:38:08 +0000

    Layout looking nice

--- js/init.js
+++ js/init.js
@@ -4,3 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems, options);
 });
+
+$( ".datepickerEarliest" ).datepicker({});
+
+$( ".selector" ).datepicker({
+  maxDate: "+1m +1w"
+});
+
+$( ".datepickerLatest" ).datepicker({});
+
+
+
+$('.timepicker').timepicker({
+    timeFormat: 'h:mm p',
+    interval: 15,
+    minTime: '12:00am',
+    maxTime: '11:45pm',
+    defaultTime: '11',
+    startTime: '00:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+});
