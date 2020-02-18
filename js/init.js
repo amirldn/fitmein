ada1ac7052347b406476a8125a9fc36a5b20251a
b66957am <amir.maula@student.manchester.ac.uk>
2020-02-18 15:39:38 +0000

    added date picker

--- js/init.js
+++ js/init.js
@@ -4,7 +4,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems, options);
 });
-
-$( function() {
-  $( "#datepicker" ).datepicker();
-} );
