34f550d4375ca0fb5472100e378d7ca2bb9bf4f0
b66957am <amir.maula@student.manchester.ac.uk>
2020-02-18 15:06:10 +0000

    added js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
