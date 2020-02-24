d772e1473b385aece55b28f50ff782770052f2d7
b66957am <amir.maula@student.manchester.ac.uk>
2020-02-24 19:31:26 +0000

    Removed autocomplete

--- js/init.js
+++ js/init.js
@@ -30,9 +30,7 @@ $('.timepicker').timepicker({
 
 //James
 function myembedjs(){
-  var inputName = document.getElementById('pac-input').value;
-  var retrievedPlaceID = undefined;
-  placeIDReturn();
+  // var inputName = document.getElementById('pac-input').value;
 
   // NEED TO GET PLACE ID FROM THE INPUTNAME ADDRESS
   alert(findBestTime(new Date(2020, 1, 2), new Date(2020, 1, 04), 9, 17, 60, "ChIJeziKgJKxe0gR8qUIiSmWKJo", "AIzaSyACHAZEZeyYI36Dxezeq9axe-GJC_BIDpU"));
