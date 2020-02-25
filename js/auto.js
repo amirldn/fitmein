d3e8b36b7e71587b9030a21ca435d0c229dc19ff
b66957am <amir.maula@student.manchester.ac.uk>
2020-02-25 13:11:26 +0000

    Fixed returning of business types in autocomplete

--- js/auto.js
+++ js/auto.js
@@ -12,7 +12,7 @@ function initAutocomplete() {
   // Create the autocomplete object, restricting the search predictions to
   // geographical location types.
   autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autocomplete'), {types: ['geocode']});
+      document.getElementById('autocomplete'), {types: ['establishment']});
 
   // Avoid paying for data that you don't need by restricting the set of
   // place fields that are returned to just the address components.
