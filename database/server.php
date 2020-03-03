2e4112efb6690b351f6e9619fb7dad18b7d1479b
b66957am <amir.maula@student.manchester.ac.uk>
2020-03-03 14:27:16 +0000

    db update 2

--- database/server.php
+++ database/server.php
@@ -34,10 +34,10 @@ $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 //form validation
 
-if(empty($username)) {array_push($errors, "Username is required")};
-if(empty($email)) {array_push($error, "Email is required")};
-if(empty($password_1)) {array_push($errors, "Password is required")};
-if($password_1 != $password_2){array_push($errors, "Passwords do not match")};
+// if(empty($username)) {array_push($errors, "Username is required")};
+// if(empty($email)) {array_push($error, "Email is required")};
+// if(empty($password_1)) {array_push($errors, "Password is required")};
+// if($password_1 != $password_2){array_push($errors, "Passwords do not match")};
 
 
 // check db for existing user with same Username
