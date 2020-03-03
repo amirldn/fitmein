4ac310a1a55a5e086c3d5dac0ea058631c34d206
b66957am <amir.maula@student.manchester.ac.uk>
2020-03-03 14:44:04 +0000

    updated directory

--- database/index.php
+++ database/index.php
@@ -5,14 +5,14 @@ session_start();
 if(isset($_SESSION['username'])){
 
   $_SESSION['msg'] = "You must log in first to view this page";
-  header("location : login.php");
+  header("location : ../login.php");
 }
 
 if(isset($_GET['logout'])){
 
   session_destroy();
   unset($_SESSION['username']);
-  header("location : login.php");
+  header("location : ../login.php");
 }
 ?>
 <!DOCTYPE html>
