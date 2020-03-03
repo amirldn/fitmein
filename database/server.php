376b6b5fcf8e71b85a52f66fc81c3175acac9a37
b66957am <amir.maula@student.manchester.ac.uk>
2020-03-03 13:53:41 +0000

    ethan test

--- database/server.php
+++ database/server.php
@@ -16,7 +16,7 @@ connect to database")
 
 //Register users
 
-$username = mysqli_real_escape_string($db, $_POST['username']);
+// $username = mysqli_real_escape_string($db, $_POST['username']);
 $email = mysqli_real_escape_string($db, $_POST['email]');
 $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
